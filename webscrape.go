293e15362495d8c0e26396290e1c033a90a0c748
Dahveyea <dahveyea.cowan@gmail.com>
2021-03-03 17:31:10 -0800

    Changed websites and refactored the code

--- webscrape.go
+++ webscrape.go
@@ -10,8 +10,8 @@ import (
 )
 
 func main() {
-    fetchURL := "https://www.crunchyroll.com/"
-    fileName := "Featured-Anime-Shows.csv"
+    fetchURL := "https://www.imdb.com/list/ls033398199/"
+    fileName := "Anime-Shows.csv"
     file, err := os.Create(fileName)
     if err != nil {
         log.Fatal("ERROR: Could not create file %q: %s\n", fileName, err)
@@ -22,7 +22,8 @@ func main() {
     defer writer.Flush()
 
     // Write column headers of the text file
-    writer.Write([]string{"Show Name", "Featured Shows","Videos",})
+    writer.Write([]string{"Sl. No.", "Movie Name", "Release Year", "Certificate", "Genre",
+		"Running time", "Rating", "Number of Votes", "Gross"})
 
     // Instantiate the default Collector
     c := colly.NewCollector()
@@ -31,22 +32,36 @@ func main() {
     c.OnRequest(func(r *colly.Request) {
         fmt.Println("Visiting: ", r.URL)
     })
+    c.OnError(func(_ *colly.Response, err error) {
+      panic(err)
+})
 
     // Callback when colly finds the entry point to the DOM segment having a show info
-    c.OnHTML(`.portrait-grid cf`, func(e *colly.HTMLElement) {
-        //Locate and extract different pieces information about each show
-        name := e.ChildText(".series-title block ellipis")
-        videos := e.ChildText(".series-title block ellipis")
-        featured_shows := e.ChildText(".landscape-grid shows")
+    c.OnHTML(`.lister-item-content`, func(e *colly.HTMLElement) {
+		//Locate and extract different pieces information about each movie
+		number := e.ChildText(".lister-item-index")
+		name := e.ChildText(".lister-item-index ~ a")
+		year := e.ChildText(".lister-item-year")
+		runtime := e.ChildText(".runtime")
+		certificate := e.ChildText(".certificate")
+		genre := e.ChildText(".genre")
+		rating := e.ChildText("[class='ipl-rating-star small'] .ipl-rating-star__rating")
+		vote := e.ChildAttr("span[name=nv]", "data-value")
+		gross := e.ChildText(".text-muted:contains('Gross') ~ span[name=nv]")
 
-
-        // Write all scraped pieces of information to output text file
-        writer.Write([]string{
-            name,
-            videos,
-            featured_shows,
-        })
-    })
+		// Write all scraped pieces of information to output text file
+		writer.Write([]string{
+			number,
+			name,
+			year,
+			certificate,
+			runtime,
+			genre,
+			rating,
+			vote,
+			gross,
+		})
+	})
 
     // start scraping the page under the given URL
     c.Visit(fetchURL)
